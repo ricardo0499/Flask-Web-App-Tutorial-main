226a346ded7232e3c19fb7d4554648c1d8e9ea88
ricardo0499 <pittiricardo7@gmail.com>
2022-06-09 13:46:28 -0500

    userid

--- website/models.py
+++ website/models.py
@@ -8,7 +8,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     report_id = db.Column(db.Integer, db.ForeignKey('reporte.id'))
 
 
